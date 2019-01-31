06db76bc7b0b9509cb3f9b454865fc83daaa37fb
vanessa <nessastagg@gmail.com>
2019-01-31 15:01:22 -0200

    teste layout alternativo index

--- teste.php
+++ teste.php
@@ -75,162 +75,31 @@
     </div>
   </nav>
 
+  <section class="section">
+    <div class="container">
 
-<div class="button-group filters-button-group">
-  <button class="button is-checked" data-filter="*">original order</button>
-  <button class="button" data-filter=".noticias">noticias</button>
-  <button class="button" data-filter=".reviews">reviews</button>
-</div>
+    <div class="grid">
 
-
-<div class="grid">
-    
-  <div class="grid-item noticias">
-    <div class="">
-      <article class="media media-card-posts">
-        <figure class="media-left media-image">
-          <p class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </p>
-        </figure>
-        <div class="media-content">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <strong class="media-title">Titulo do Post</strong>
           <div class="content">
-            <p class="media-spacing">
-              <span class="tags is-pulled-right media-tags">
-                <span class="tag is-dark">Noticias</span><span class="tag is-dark">Mobile</span>
-              </span>
-              <strong class="media-title">BTitulo do Post</strong> <br>
-              <small>Em 26/01/2018</small> <small>por <a href="">Yuck</a></small>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Venenatis cras sed felis eget. A scelerisque purus semper eget. Lectus nulla at volutpat diam ut venenatis tellus in metus.
-              <a href="">Leia mais</a>
-            </p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Phasellus nec iaculis mauris.
           </div>
         </div>
-        <div class="media-right">
-        </div>
-     </article>
-    </div>
-  </div>
+      </div>
 
-  <div class="grid-item reviews">
-    <div class="">
-      <article class="media media-card-posts">
-        <figure class="media-left media-image">
-          <p class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </p>
-        </figure>
-        <div class="media-content">
-          <div class="content">
-            <p class="media-spacing">
-              <span class="tags is-pulled-right media-tags">
-                <span class="tag is-dark">Reviews</span><span class="tag is-dark">Mobile</span>
-              </span>
-              <strong class="media-title">ATitulo do Post</strong> <br>
-              <small>Em 26/01/2018</small> <small>por <a href="">Yuck</a></small>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Venenatis cras sed felis eget. A scelerisque purus semper eget. Lectus nulla at volutpat diam ut venenatis tellus in metus.
-              <a href="">Leia mais</a>
-            </p>
-          </div>
-        </div>
-        <div class="media-right">
-        </div>
-     </article>
     </div>
-  </div>
 
-  <div class="grid-item noticias">
-   <div class="">
-      <article class="media media-card-posts">
-        <figure class="media-left media-image">
-          <p class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </p>
-        </figure>
-        <div class="media-content">
-          <div class="content">
-            <p class="media-spacing">
-              <span class="tags is-pulled-right media-tags">
-                <span class="tag is-dark">Noticias</span><span class="tag is-dark">Mobile</span>
-              </span>
-              <strong class="media-title">VTitulo do Post</strong> <br>
-              <small>Em 26/01/2018</small> <small>por <a href="">Yuck</a></small>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Venenatis cras sed felis eget. A scelerisque purus semper eget. Lectus nulla at volutpat diam ut venenatis tellus in metus.
-              <a href="">Leia mais</a>
-            </p>
-          </div>
-        </div>
-        <div class="media-right">
-        </div>
-     </article>
     </div>
-  </div>
+  </section>
 
-  <div class="grid-item reviews">
-    <div class="">
-      <article class="media media-card-posts">
-        <figure class="media-left media-image">
-          <p class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </p>
-        </figure>
-        <div class="media-content">
-          <div class="content">
-            <p class="media-spacing">
-              <span class="tags is-pulled-right media-tags">
-                <span class="tag is-dark">Reviews</span><span class="tag is-dark">Mobile</span>
-              </span>
-              <strong class="media-title">DTitulo do Post</strong> <br>
-              <small>Em 26/01/2018</small> <small>por <a href="">Yuck</a></small>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Venenatis cras sed felis eget. A scelerisque purus semper eget. Lectus nulla at volutpat diam ut venenatis tellus in metus.
-              <a href="">Leia mais</a>
-            </p>
-          </div>
-        </div>
-        <div class="media-right">
-        </div>
-     </article>
-    </div>
-  </div>
-
-  <div class="grid-item noticias">
-    <div class="">
-      <article class="media media-card-posts">
-        <figure class="media-left media-image">
-          <p class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </p>
-        </figure>
-        <div class="media-content">
-          <div class="content">
-            <p class="media-spacing">
-              <span class="tags is-pulled-right media-tags">
-                <span class="tag is-dark">Noticias</span><span class="tag is-dark">Mobile</span>
-              </span>
-              <strong class="media-title">QTitulo do Post</strong> <br>
-              <small>Em 26/01/2018</small> <small>por <a href="">Yuck</a></small>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Venenatis cras sed felis eget. A scelerisque purus semper eget. Lectus nulla at volutpat diam ut venenatis tellus in metus.
-              <a href="">Leia mais</a>
-            </p>
-          </div>
-        </div>
-        <div class="media-right">
-        </div>
-     </article>
-    </div>
-  </div>
-
-</div>
 
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
