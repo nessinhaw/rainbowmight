6b38f7d734f3e64bcc85debf3c639c43902c187c
Vanessa <nessastagg@gmail.com>
2019-02-05 04:00:41 -0200

    layout pagina inicial definido

--- js/teste.js
+++ js/teste.js
@@ -1,24 +1,26 @@
-// init Isotope
-$('.grid').isotope({
+$('.grid').masonry({
   itemSelector: '.grid-item',
-  masonry: {
-    columnWidth: 360,
-    isFitWidth: true,
-    gutter: 10
-  }
+  columnWidth: 365,
+  isFitWidth: true,
+  gutter: 10,
+  horizontalOrder: true
 });
-// bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  //filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
+
+$('.carousel').flickity({
+  // options
+  cellAlign: 'center',
+  wrapAround: true,
+  imagesLoaded: true,
+  setGallerySize: false
 });
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
+
+
+$("a#toggle").click(function(){
+   	$(".footer").slideToggle();
+    return false;
+}); 
+
+$("a#toggle2").click(function(){
+   	$(".search-bar").slideToggle();
+    return false;
+}); 
--- teste.php
+++ teste.php
@@ -6,16 +6,64 @@
     <title>Hello Bulma!</title>
     <link href="https://fonts.googleapis.com/css?family=Major+Mono+Display|Playfair+Display|Roboto:300" rel="stylesheet">
     <link rel="stylesheet" href="bulma-0.7.2/css/bulma.min.css">
+    <link rel="stylesheet" href="css/flickity.min.css">
     <link rel="stylesheet" href="css/teste.css">
 
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
   </head>
   <body>
 
+  <div class="footer">
+    <div class="columns">
+      <div class="column">
+        <h1>Quem escreve?</h1>
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-48x48">
+              <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>Nome do Autor</strong>
+                <br>
+                Breve introdução.
+              </p>
+            </div>
+          </div>
+        </article>
+
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-48x48">
+              <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>Nome do Autor</strong>
+                <br>
+                Breve introdução.
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        Somethiiiing
+      </div>
+      <div class="column">
+        Other Somethiiiing
+      </div>
+    </div>
+  </div>
+
   <nav class="navbar is-light" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item site-logo" href="https://bulma.io">
-        Mana Hungry
+      <a class="navbar-item" href="https://bulma.io">
+        Nameless
       </a>
 
       <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -27,36 +75,39 @@
 
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
-        <a class="navbar-item navbar-teste">
+        <a class="navbar-item">
           Home
         </a>
 
-        <a class="navbar-item navbar-teste">
+        <a class="navbar-item">
           Notícias
         </a>
 
-        <a class="navbar-item navbar-teste">
+        <a class="navbar-item">
           Reviews
         </a>
 
-        <a class="navbar-item navbar-teste">
+        <a class="navbar-item">
           Guias
         </a>
 
-        <a class="navbar-item navbar-teste">
+        <a class="navbar-item">
           Tecnologia
         </a>
 
-        <a class="navbar-item navbar-teste">
+        <a class="navbar-item">
           Eventos
         </a>
 
+        <a href="#" id="toggle" class="navbar-item">
+          Sobre
+        </a>
       </div>
 
       <div class="navbar-end">
         <div class="navbar-item navbar-teste">
           <p class="buttons">
-            <a class="button is-small">
+            <a href="" id="toggle2" class="button is-small">
               <span class="icon">
                 <i class="fas fa-search"></i>
               </span>
@@ -75,71 +126,152 @@
     </div>
   </nav>
 
+  <div class="search-bar has-text-centered">
+    <input class="input" type="text" placeholder="Pesquisar...">
+    <a class="button">
+      <span class="icon is-small">
+        <i class="fas fa-search"></i>
+      </span>
+    </a>
+  </div>
+
+  <div class="carousel">
+    <div class="carousel-cell">
+      <div class="is-featured">
+        <img src="https://i1.wp.com/bloody-disgusting.com/wp-content/uploads/2019/01/resident-evil-2-remake-music-cover.jpg?w=1200&ssl=1" alt="Placeholder image">
+        <div class="overlay">
+          <a href="" class="tag is-dark">Reviews</a>
+          <h1>Titulo do Post</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. <a href="">Leia mais</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-cell">
+      <div class="is-featured">
+        <img src="https://www.playreplay.com.br/wp-content/uploads/2018/03/elder_scrolls_online_summerset_isles-7.jpg" alt="Placeholder image">
+        <div class="overlay">
+          <a class="tag is-dark">Reviews</a>
+          <h1>Titulo do Post</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. <a href="">Leia mais</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-cell">
+      <div class="is-featured">
+        <img src="https://i.ytimg.com/vi/G7-msC73Bsw/maxresdefault.jpg" alt="Placeholder image">
+        <div class="overlay">
+          <a class="tag is-dark">Reviews</a>
+          <h1>Titulo do Post</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. <a href="">Leia mais</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <section class="section">
-          <div class="grid">
+
+          <div id="grid" class="grid">
             <div class="grid-sizer"></div>
 
-            <div class="grid-item">
+            <div class="grid-item noticias">
               <div class="card">
                 <div class="card-image">
                   <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                    <img src="https://i1.wp.com/bloody-disgusting.com/wp-content/uploads/2019/01/resident-evil-2-remake-music-cover.jpg?w=1200&ssl=1" alt="Placeholder image">
                   </figure>
                 </div>
                 <div class="card-content">
-                  <strong class="media-title">Titulo do Post</strong>
+                  <a class="tag is-dark">Reviews</a>
+                  <h1><a href="">Titulo do Post</a></h1> <small>04/02 por <a href="">Autor</a></small>
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. <a href="">Leia mais</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="grid-item reviews">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://www.playreplay.com.br/wp-content/uploads/2018/03/elder_scrolls_online_summerset_isles-7.jpg" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <a class="tag is-dark">Notícias</a>
+                  <h1><a href="">Titulo do Post</a></h1> <small>04/02 por <a href="">Autor</a></small>
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. <a href="">Leia mais</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="grid-item guias">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://i.ytimg.com/vi/G7-msC73Bsw/maxresdefault.jpg" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <a class="tag is-dark">Guias</a>
+                  <h1><a href="">Titulo do Post</a></h1> <small>03/02 por <a href="">Autor</a></small>
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. <a href="">Leia mais</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="grid-item noticias">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://static.altchar.com/live/media/images/950x633_ct/11833_Metro_exodus_7738dae0c72e3d877079d72679581a4e.jpg" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <a class="tag is-dark">Notícias</a>
+                  <h1><a href="">Titulo do Post</a></h1> <small>02/02 por <a href="">Autor</a></small>
                   <div class="content">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
+                    Phasellus nec iaculis mauris. <a href="">Leia mais</a>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div class="grid-item">
+            <div class="grid-item noticias">
               <div class="card">
                 <div class="card-image">
                   <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                    <img src="https://i.redd.it/2wtju8wdor101.jpg" alt="Placeholder image">
                   </figure>
                 </div>
                 <div class="card-content">
-                  <strong class="media-title">Titulo do Post</strong>
-                  <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet.
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="grid-item">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <strong class="media-title">Titulo do Post</strong>
-                  <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet.
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="grid-item">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <strong class="media-title">Titulo do Post</strong>
+                  <a class="tag is-dark">Reviews</a>
+                  <h1><a href="">Titulo do Post</a></h1> <small>02/02 por <a href="">Autor</a></small>
                   <div class="content">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
+                    Phasellus nec iaculis mauris. <a href="">Leia mais</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="grid-item reviews">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://s3.dexerto.com/thumbnails/_thumbnailLarge/Is-Fallout-76-down-Bethesda-confirms-downtime-for-nuke-silo-hot-fix-January-2.jpg" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <a class="tag is-dark">Notícias</a>
+                  <h1><a href="">Titulo do Post</a></h1> <small>01/02 por <a href="">Autor</a></small>
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. <a href="">Leia mais</a>
                   </div>
                 </div>
               </div>
@@ -150,6 +282,8 @@
 
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+  <script src="js/masonry.pkgd.min.js"></script>
+  <script src="js/flickity.pkgd.min.js"></script>
   <script src="js/teste.js"></script>
   </body>
 </html>
